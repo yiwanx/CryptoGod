9090132b387a3affa235edca2f4bbd4343722a8c
Ivan Petukhov <i.petukhov@octopod.ru>
2021-12-25 15:33:00 +0300

    Initial Commit

---
+++ CryptoGod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoGod
+//
+//  Created by Ivan Petukhov on 25.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoGod/CryptoGodApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoGodApp.swift
+//  CryptoGod
+//
+//  Created by Ivan Petukhov on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoGodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
